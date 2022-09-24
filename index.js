fd01d7ef7b632e6a44565457823e61e1e37f2ecb
dungnn1711 <87370857+dungnn1711@users.noreply.github.com>
2022-09-25 02:45:20 +0700

    update

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ dotenv.config();
 
 // Create express app
 const app = express();
-const PORT = process.env.YOUR_PORT || process.env.$PORT || 5000;
-const HOST = process.env.YOUR_HOST || process.env.$HOST || '0.0.0.0';
+const PORT = process.env.YOUR_PORT || process.env.PORT || 5000;
+const HOST = process.env.YOUR_HOST || process.env.HOST || '0.0.0.0';
 
 // Middlewares
 app.use(bodyParser.json({ limit: '30mb' }));
